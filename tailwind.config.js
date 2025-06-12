cf63ccd1b86c6c570be95b8b87c43956acddc36e
loganrw <loganrwatkins@hotmail.com>
2025-06-11 19:21:48 -0500

    play field changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,6 @@ module.exports = {
   },
   theme: {
     extend: {
-      screens: {
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-      },
       colors: {
         'ga-blue': '#2F7AB1',
         'ga-blue-light': '#368ecf'
